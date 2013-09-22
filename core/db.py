91c5b8ba559fea36c9f2d151a041944c79a80e3a
Emilio Pinna <emilio.pinn@gmail.com>
2013-09-22 23:10:45 +0200

    Dirty conversion from mongodb found recotd to json. TODO: rewrite it

--- core/db.py
+++ core/db.py
@@ -3,8 +3,11 @@ try:
 except ImportError, e:
     from pymongo import Connection
 
+from bson import json_util
 from core.validation import validate, sanitize_json
 from core.config import collections
+import json
+
 
 connection = Connection('localhost', 27017)
 db = connection.ts
@@ -13,15 +16,21 @@ db = connection.ts
 for missing_collection in (set(collections) - set(db.collection_names())):
     db[missing_collection]
 
-# Get selected records from collection
+# Get selected records from collection, and return it as json
 # Called by GET /<document>/
 def get(collection, selection = {}):
-    return db[collection].find(sanitize_json(selection))
+    cursors = db[collection].find(sanitize_json(selection))
+    json_out = []
+    for cursor in cursors:
+        del cursor['_id']
+        json_out.append(cursor)
+    return json_out
+
 
 # Remove selected records from collection
-# Called by POST /del/<collection>
+# Called by POST /remove/<collection>
 def remove(collection, selection = {}):
-    return db[collection].remove(sanitize_json(selection))
+    db[collection].remove(sanitize_json(selection))
 
 # Insert new record to collection
 # Called by POST /add/<collection>/
