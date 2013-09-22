41c5dd7d20e9f0a3deca16eebaada1788a328b92
Emilio Pinna <emilio.pinn@gmail.com>
2013-09-22 17:33:31 +0200

    Added first database schema and first API stubs

--- core/config.py
+++ core/config.py
@@ -9,7 +9,12 @@ www_folder = os.path.abspath(os.path.join(root_folder,'www'))
 templates_folder = os.path.abspath(os.path.join(www_folder,'templates'))
 views_folder = os.path.abspath(os.path.join(www_folder,'views'))
 
-# Load yaml file
+# Load database schema validation from schema.yaml
+yaml_path = os.path.join(core_folder, 'schema.yaml')
+schema = yaml.load(file(yaml_path, 'r'))
+collections = schema.keys()
+
+# Load configuration from config.yaml
 yaml_path = os.path.join(core_folder, 'config.yaml')
 conf = yaml.load(file(yaml_path, 'r'))
 
@@ -18,7 +23,7 @@ conf['static']['/'] = {}
 conf['static']['/']['tools.staticdir.root'] = root_folder
 # conf['static']['tools.staticdir.dir'] = os.path.abspath(path.join())
 
-# Save it as accessible dicts
+# Save conf as accessible dicts
 conf_server = conf['server']
 conf_static = conf['static']
 conf_auth = conf['auth']
---
+++ core/validation.py
@@ -0,0 +1,23 @@
+import cgi, validictory, yaml, os
+from core.config import schema, core_folder
+
+
+# Input validation method to prevent XSS 
+def sanitize_json(json):
+    
+    sanitized_json = {}
+    for key, value in json.items:
+        sanitized_json[key] = cgi.escape(value, quote=True)
+        
+    return sanitized_json
+
+
+# Validate against schema saved in yaml
+def _validate_schema(document, json):
+    validictory.validate(json, schema[document])
+    
+
+# JSON validation method
+def validate(document, json):
+    _validate_schema(document, json)
+    return sanitize_json(json)
