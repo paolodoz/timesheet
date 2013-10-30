fd2b23f000933c268871ee75a44e5a9fef1b7629
Emilio Pinna <emilio.pinn@gmail.com>
2013-10-30 21:41:23 +0100

    Minor cleanup

--- core/permissions.py
+++ core/permissions.py
@@ -57,9 +57,6 @@ def check_criteria_permissions(collection, criteria):
         # If not, skip procedure
         return
     
-    print 'VALIDO', criteria
-    print 'CONTRO', restrictions_criteria
-
     jsonschema.validate(criteria, restrictions_criteria, format_checker=jsonschema.FormatChecker())
 
    
