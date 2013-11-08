72a94961a52c5a20c46f1cba28513ebc4e70329e
Emilio Pinna <emilio.pinn@gmail.com>
2013-11-08 11:09:58 +0100

    Add secure HTTP headers

--- timesheet.py
+++ timesheet.py
@@ -4,10 +4,18 @@ import cherrypy, logging
 from core.routes.routes import Routes
 from core.config import conf_server, conf_static, conf_logging
 
+def secureheaders():
+    headers = cherrypy.response.headers
+    headers['X-Frame-Options'] = 'DENY'
+    headers['X-XSS-Protection'] = '1; mode=block'
+    headers['Content-Security-Policy'] = "default-src='self'"
+
+
 if __name__ == '__main__':
     
     # Update configurations
     cherrypy.config.update(conf_server)
+    cherrypy.tools.secureheaders = cherrypy.Tool('before_finalize', secureheaders, priority=60)
     
     for logname, loglevel in conf_logging.items():
         logging_level = getattr(logging, loglevel)
--- timesheet_apache.py
+++ timesheet_apache.py
@@ -9,6 +9,12 @@ sys.path.insert(0, installation_path)
 from core.routes.routes import Routes
 from core.config import conf_server, conf_static, conf_logging
 
+def secureheaders():
+    headers = cherrypy.response.headers
+    headers['X-Frame-Options'] = 'DENY'
+    headers['X-XSS-Protection'] = '1; mode=block'
+    headers['Content-Security-Policy'] = "default-src='self'"
+
 # Update configurations
 cherrypy.config.update(conf_server)
 cherrypy.config.update({'environment': 'embedded'})
@@ -21,6 +27,8 @@ for logname, loglevel in conf_logging.items():
     logging_level = getattr(logging, loglevel)
     cherrypy_log = getattr(cherrypy.log, logname)
     cherrypy_log.setLevel(logging_level)
+
+cherrypy.tools.secureheaders = cherrypy.Tool('before_finalize', secureheaders, priority=60)
 
 routes = Routes()
 application = cherrypy.Application(routes, config=conf_static)
