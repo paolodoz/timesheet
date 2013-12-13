290585222627a5ffecc3b038ce42abf3402b5648
Emilio Pinna <emilio.pinn@gmail.com>
2013-12-13 17:10:43 +0100

    Changes to log output also in embedded enviroinments with apache2 mod_wsgi

--- timesheet_apache.py
+++ timesheet_apache.py
@@ -3,7 +3,8 @@
 import cherrypy, logging
 import sys, os
 
-installation_path = os.path.dirname(__file__)
+# Suppose this file in 
+installation_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 sys.path.insert(0, installation_path)
 from core.routes.routes import Routes
@@ -16,8 +17,15 @@ def secureheaders():
     headers['Content-Security-Policy'] = "default-src='self'"
 
 # Update configurations
+# If log paths are absolute, move to current path
+if not os.path.isabs(conf_server['log.access_file']):
+    conf_server['log.access_file'] = os.path.join(installation_path, conf_server['log.access_file'])
+if not os.path.isabs(conf_server['log.error_file']):
+    conf_server['log.error_file'] = os.path.join(installation_path, conf_server['log.error_file'])
+    
+conf_server['environment'] = 'embedded'
 cherrypy.config.update(conf_server)
-cherrypy.config.update({'environment': 'embedded'})
+    
 
 if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
     cherrypy.engine.start(blocking=False)
